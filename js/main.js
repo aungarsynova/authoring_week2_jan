b276ff1abfebad858be58a5f4f047379573f9c65
unknown <you@example.com>
2019-01-17 16:39:10 -0500

    components

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
 (()=> {
-   
+   //create a components first
+    const UserComponent = {
+        props: ['name', 'role'],
+
+        template: "#userstemplate",
+
+        created: function() {
+            console.log("let me diiiiiiiiiiiiiiie");
+        },
+
+        methods: {
+            logFromChild() {
+                console.log("logged from the component");
+            },
+
+            passEvent() {
+                this.$emit('shoutup');
+            }
+        }
+    };
+
+   //then your vue instance
+   const vm = new Vue({
+       el: "#app",
+
+       data: {
+           testmessage: "sup"
+       },
+
+       methods: {
+            calledOnParent() {
+                console.log("This method lives in the main VM and was called froma  component");
+            }
+       },
+
+       created: function() {
+        console.log("this is the main vue intance");
+       },
+
+       components: {
+           'activeusers' : UserComponent,
+       }
+   })
 })();
